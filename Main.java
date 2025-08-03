789fcf961a8f8286b99239279ac82553bee6a7db
Shubham <shubhamw0343@gmail.com>
2025-08-03 16:06:37 +0530

    first commit

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
         System.out.println("This is java program");
         System.out.println("output of java program");
         System.out.println(new Date());
+        System.out.println("Git commit");
     }
 }
